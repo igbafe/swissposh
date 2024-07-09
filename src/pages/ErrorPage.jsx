91512b9e258c0604dcdebdef104fd8b7cc829102
igbafe peter <igbafes8@gmail.com>
2024-07-09 21:00:21 +0100

    home

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function ErrorPage() {
   return (
     <div className=" h-full w-full flex justify-center items-center">
       <div className="flex h-[435px] w-[211px] gap-3 md:w-[432px] flex-col justify-center text-center items-center">
-        <p className="md:text-[150px] text-[73px] text-[#F20000] font-black">Oops</p>
+        <p className="md:text-[150px] text-[73px] text-[#F20000] font-black">
+          Oops
+        </p>
         <p className="md:text-[26px] text-[13px] font-black text-[#001F3F]">
           404-Page Not Found
         </p>
@@ -12,7 +15,10 @@ function ErrorPage() {
           "Looks like you've found yourself on a lost page. " or "The page you
           requested was not found."
         </p>
-        <button className="md:h-[40px] h-[23px] w-[133px] md:w-[222px] md:text-[16px] text-[9px] bg-[#001F3F] text-white">Return To Homepage</button>
+        <button className="md:h-[40px] h-[23px] w-[133px] md:w-[222px] md:text-[16px] text-[9px] bg-[#001F3F] text-white">
+          {" "}
+          <Link to="/"> Return To Homepage</Link>
+        </button>
       </div>
     </div>
   );
